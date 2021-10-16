ee8b663db0409b60ff771c9d2cf2125c2e2e2a90
ACmyles <77598844+ACmyles@users.noreply.github.com>
2021-10-16 15:05:24 +1100

    refactor, add _ to local functions, full key:value store dictionary implementation w/ .gitignore, update readme

--- README.md
+++ README.md
@@ -1,40 +1,45 @@
 # pyvenv-fast
 
-A zsh plugin to launch a Python venv with one command.
-
-Designed for use with [dotpyvenv](https://github.com/jeanpantoja/dotpyvenv) (auto venv activation on navigation).
+A zsh plugin to launch a Python venv.  Designed with [dotpyvenv](https://github.com/jeanpantoja/dotpyvenv) in mind.
 
 ## Features
 
 - Launch a Python venv in current directory or mkcd a new directory.
-- Set venv name the same as directory with a dot before - eg `.new_proj`
 - Create .pyvenvdir to point to venv directory.
 - Specify Python version.
+- Add venv to .gitignore.
 
 ## Installation
 
 ### Oh My Zsh
 
 ```shell
-git clone https://github.com/ACmyles/pyvenv-fast.git ~/.oh-my-zsh/custom/plugins/pyvenv-fast
+git clone https://github.com/ACmyles/pyvenv-fast.git $ZSH_CUSTOM/plugins/pyvenv-fast
 ```
 
-Add pyvenv-fast to plugins in `.zshrc`
+Add pyvenv-fast to plugins in `.zshrc`.
 
 ## Usage
 
 ```shell
-pv [-v] directory
+pv [-g] [-v ...] directory
 ```
 
-## Example Usage
+### Options
+
+| Option | Argument | Purpose |
+| ----- | ----- | -------- |
+| `-g`  | _None_ | Append new venv to .gitignore.
+| `-v`  | python 3.x | Specify Python version.
+
+### Example Usage
 
 ```shell
-pv .
+pv new_project
 
-pv new_proj
+pv -g .
 
 pv -v python3.7 .
 
-pv -v python3.7 new_proj
+pv -g -v python3.7 new_project
 ```
--- pyvenv-fast.plugin.zsh
+++ pyvenv-fast.plugin.zsh
@@ -1,54 +1,58 @@
-function pv_GenerateGitIgnore(){
+# appends .pyvenvdir and the pyvenv directory to .gitignore
+function _pv_AppendToGitIgnore(){
     echo ".pyvenvdir" >> .gitignore
-    echo "/.${call_state[pv_directory]}" >> .gitignore
+    echo "/.${pv_call_keys[pv_directory]}" >> .gitignore
 }
 
-function pv_CreatePyVenvInCurrentDirectory(){
-    ${call_state[python_version]} -m venv ".${PWD##*/}"
+# creates a pyvenv in the working directory
+function _pv_CreatePyVenvInWorkingDirectory(){
+    ${pv_call_keys[python_version]} -m venv ".${PWD##*/}"
     echo ".${PWD##*/}" >> .pyvenvdir
 }
 
-function pv_CreatePyVenvInNewDirectory(){
-    mkdir -p ${call_state[pv_directory]}
-    cd ${call_state[pv_directory]}
-    ${call_state[python_version]} -m venv ".${call_state[pv_directory]}"
-    echo ".${call_state[pv_directory]}" >> .pyvenvdir
+# creates a pyvenv in a brand new directory (uses mkcd)
+function _pv_CreatePyVenvInNewDirectory(){
+    mkdir -p ${pv_call_keys[pv_directory]}
+    cd ${pv_call_keys[pv_directory]}
+    ${pv_call_keys[python_version]} -m venv ".${pv_call_keys[pv_directory]}"
+    echo ".${pv_call_keys[pv_directory]}" >> .pyvenvdir
 }
 
 function pv(){
-    declare -A local call_state
-    call_state[pv_directory]=${argv[-1]}
-    call_state[python_version]="python3.10"
-    call_state[git_ignore]=false
-    
-    
-    # exit if no args
+    # exit if no arguments provided
     if [ ! ${1} ]; then
-        echo "usage: pv [-v] directory"
+        echo "usage: pv [-g] [-v ...] directory"
         return
     fi
     
-    # check for flags
+    # create call state dictionary key:value store
+    declare -A local pv_call_keys
+    pv_call_keys[pv_directory]=${argv[-1]}
+    pv_call_keys[python_version]="python3.10"
+    pv_call_keys[git_ignore]=false
+    
+    # evaluate flags
     while getopts 'v:g' OPTION; do
         case "${OPTION}" in
             v) # python version flag -v
-                call_state[python_version]=${OPTARG}
-                echo "Using ${call_state[python_version]}..."
+                pv_call_keys[python_version]=${OPTARG}
+                echo "Using ${pv_call_keys[python_version]}..."
             ;;
             g) # git ignore append flag -g
-                call_state[git_ignore]=true
+                pv_call_keys[git_ignore]=true
             ;;
         esac
     done
     
-    # call in current or new dir
-    if [ "${call_state[pv_directory]}" = "." ]; then
-        pv_CreatePyVenvInCurrentDirectory
+    # evaluate directory
+    if [ "${pv_call_keys[pv_directory]}" = "." ]; then
+        _pv_CreatePyVenvInWorkingDirectory
     else
-        pv_CreatePyVenvInNewDirectory ${call_state[pv_directory]}
+        _pv_CreatePyVenvInNewDirectory ${pv_call_keys[pv_directory]}
     fi
     
-    if [ ${call_state[git_ignore]} ]; then
-        pv_GenerateGitIgnore
+    # append .pyvenvdir and the new py venv to .gitignore on -g flag
+    if [ ${pv_call_keys[git_ignore]} = true ]; then
+        _pv_AppendToGitIgnore
     fi
 }
